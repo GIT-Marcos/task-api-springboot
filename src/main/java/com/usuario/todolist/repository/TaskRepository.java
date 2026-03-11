2fa00f7105695af379ed1ec2165cc1bfdf3e6a8a
marcos <m.pardo.agustin@gmail.com>
2026-03-11 14:10:33 -0300

    ref gral -quitado método sin uso repo -quitada respuesta 204 de findAll sin filtros

--- src/main/java/com/usuario/todolist/repository/TaskRepository.java
+++ src/main/java/com/usuario/todolist/repository/TaskRepository.java
@@ -10,8 +10,6 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
-    List<Task> findByCompleted(Boolean isCompleted);
-
     Boolean existsByDescription(String description);
 
     Boolean existsByDescriptionAndIdNot(String description, Long id);
