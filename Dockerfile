924eb2bc581355182f94f858c84e97550bdaab43
marcos <m.pardo.agustin@gmail.com>
2026-04-02 22:18:05 -0300

    fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,11 @@ FROM otel/opentelemetry-collector-contrib:0.90.0 AS otel-bin
 FROM eclipse-temurin:21-jre-jammy
 WORKDIR /app
 
-COPY --from=otel-bin /otelcol /app/otelcol
+RUN apt-get update && apt-get install -y curl gnupg2 lsb-release
+
+RUN curl -L https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.90.0/otelcol-contrib_0.90.0_linux_amd64.deb -o otelcol.deb && \
+    dpkg -i otelcol.deb && \
+    rm otelcol.deb
 
 COPY otel-config-prod.yml /app/otel-config-prod.yml
 COPY otel-config-dev.template.yml /app/otel-config-dev.template.yml
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 if [ "$APP_PROFILE" = "prod" ]; then
     CONFIG_FILE="/app/otel-config-prod.yml"
 else
-    # En DEV, si no renombraste el template, intentamos usar el template
     if [ -f "/app/otel-config.yml" ]; then
         CONFIG_FILE="/app/otel-config.yml"
     else
@@ -10,17 +9,10 @@ else
     fi
 fi
 
-echo ">>> Verificando binario del Collector..."
-ls -l /app/otelcol
+echo ">>> Iniciando OTEL Collector desde /usr/bin/otelcol-contrib..."
+/usr/bin/otelcol-contrib --config=$CONFIG_FILE &
 
-echo ">>> Iniciando OTEL Collector..."
-/app/otelcol --config=$CONFIG_FILE &
+sleep 5
 
-sleep 3
-
-echo ">>> Iniciando Java..."
-java -jar app.jar
-
-echo "Uso de configuración: $CONFIG_FILE"
-/app/otelcol --config=$CONFIG_FILE &
+echo ">>> Iniciando Aplicación Java..."
 java -jar app.jar
